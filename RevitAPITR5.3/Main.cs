5eb056fdd1e6fbfdb02385693a93b66f411811b8
IlnurKam <kamal673@yandex.ru>
2022-05-24 23:00:50 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITR5.3/Main.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITR5._3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "Revit API training";
+            application.CreateRibbonTab(tabName);
+            string utilsFolderPath = @"C:\Program files\RevitAPITraining\";
+
+            var panel = application.CreateRibbonPanel(tabName, "Стены");
+
+            var button = new PushButtonData("Тип", "Смена типа стен", 
+                Path.Combine(utilsFolderPath, "RevitAPITraningUI.dll"), 
+                "RevitAPITraningUI.Main");
+
+            Uri uriImage = new Uri(@"C:\Program Files\RevitApiTraining\Images\RevitAPITrainingUI_32.png", UriKind.Absolute);
+            BitmapImage largeImage = new BitmapImage(uriImage);
+            button.LargeImage = largeImage;
+
+            panel.AddItem(button);
+
+            return Result.Succeeded;
+        }
+    }
+}
